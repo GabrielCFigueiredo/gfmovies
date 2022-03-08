647292b62776d379841af2637109f84f7531441a
Gabriel <gabriel.devsistema@gmail.com>
2022-03-08 19:53:01 -0300

    project created

---
+++ backend/src/test/java/com/devsuperior/gfmovie/GfmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.gfmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
