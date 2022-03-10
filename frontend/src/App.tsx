596a23225bcf77398751dfe4fcd349962f8f9316
Gabriel <gabriel.devsistema@gmail.com>
2022-03-10 09:07:57 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navBar/navBar";
+import Form from "./feature/Form";
+import Home from "./feature/Home";
 
 export default function App() {
 
   return (
-    <div>
-      <NavBar/>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/form/:id" element={<Form/>}/>
+      </Routes>
+    </BrowserRouter>
   )
   
 }
